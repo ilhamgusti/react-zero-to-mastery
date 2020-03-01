42ac6c6c64bf61120868d591d9545fe335f70732
ilhamgusti <ilham.wibowo51@gmail.com>
2020-03-01 10:12:28 +0700

    use switch, route & BrowserRouter in App.js

--- packages/crwn-clothing/src/App.js
+++ packages/crwn-clothing/src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
+function TestPage() {
+  return (<p>Test Pages</p>)
+}
 function App() {
   return (
-    <div>
-      <HomePage/> 
-    </div>
+    <Router>
+      <Switch>
+      <Route exact path="/" component={HomePage}/>
+      <Route path="/test" component={TestPage}/>
+      </Switch>
+    </Router>
   );
 }
 
